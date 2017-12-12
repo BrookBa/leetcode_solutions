48dc2c5672d07379f28d692deeda824cd1f72f8c
Brook Baise <brookba@gmail.com>
2017-12-12 12:19:45 -0800

    Update ArrayPartition.cs
    Added URL of problem and date

--- ArrayPartition.cs
+++ ArrayPartition.cs
@@ -1,3 +1,6 @@
+//Problem URL: https://leetcode.com/problems/array-partition-i/description/
+//Date Completed: 12/12/17
+
 public class Solution {
     public int ArrayPairSum(int[] nums) {
         Array.Sort(nums);
